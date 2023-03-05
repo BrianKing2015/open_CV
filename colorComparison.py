70b52fdd4cb6035dbcb73aa9e848a89a3a81f39e
Briank <brian.john.king@gmail.com>
2023-03-05 11:04:45 -0500

    Adding color comparison

---
+++ colorComparison.py
@@ -0,0 +1,43 @@
+import cv2
+import skimage
+import numpy as np
+
+class color_comparison():
+    def __init__(self, img):
+        self.img = img
+    
+    def color_histogram(self):
+        image = cv2.imread(self.img)
+        histBlue = cv2.calcHist([image],[0],None,[256],[0,256])
+        histRed = cv2.calcHist([image],[1],None,[256],[0,256])
+        histGreen = cv2.calcHist([image],[2],None,[256],[0,256])
+        return (histBlue, histRed, histGreen)
+
+
+    def mean_color(self):
+        img = skimage.io.imread(self.img)[:, :, :-1]
+        mean = img.mean(axis=0).mean(axis=0)
+        pixels = np.float32(img.reshape(-1, 3))
+        n_colors = 5
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
+        flags = cv2.KMEANS_RANDOM_CENTERS
+        _, labels, palette = cv2.kmeans(pixels, n_colors, None, criteria, 10, flags)
+        _, counts = np.unique(labels, return_counts=True)
+        dominant = palette[np.argmax(counts)]
+        return [mean, dominant]
+
+
+    def average_color(self):
+        img = cv2.imread(self.img)
+        average_color_per_row = np.average(img, axis=0)
+        average_color = np.average(average_color_per_row, axis=0)
+        return average_color
+    
+
+
+if __name__ == "__main__":
+    color = color_comparison(img='test.png')
+    print (color.average_color())
+    print (color.mean_color())
+    print (color.color_histogram())
+    
---
+++ pca_basic.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+
+def contours_pca(contours):
+    '''
+    Joins the contour points into array and removes unit values.
+    Then calculates the PCA of the contour points.
+    @param contours: list of contours
+    @return: list of (center, angle, width, height)
+    '''
+    contours_points = np.vstack(contours).squeeze().astype(np.float32)
+
+    mean, eigenvectors = cv2.PCACompute(contours_points, mean=None)
+
+    center = mean.squeeze().astype(np.int32)
+    delta = (150*eigenvectors).squeeze().astype(np.int32)
+    return center, delta
+
+def draw_pca(img, contours, center, delta):
+    '''
+    Draws the PCA on the image.
+    @param img: image to draw on
+    @param contours: list of contours
+    @param center: center of the PCA
+    @param delta: delta of the PCA
+    '''
+    cv2.line(img, tuple(center + delta[0]), tuple(center - delta[0]), (0, 255, 0), 2)
+    cv2.line(img, tuple(center + delta[1]), tuple(center + delta[1]), (0, 255, 0), 2)
+    cv2.circle(img, tuple(center), 3, (0, 0, 255), 2)
+
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture("test.mp4")
+    while True:
+        status_cap , frame = cap.read()
+        if status_cap == False:
+            break
+        frame = cv2.resize(frame,(0,0), frame, 0.5, 0.5)
+        edges = cv2.Canny(frame, 250, 150)
+        print(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        _, contours = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+        if len(contours) > 0:
+            center, delta = contours_pca(contours)
+            draw_pca(frame, contours, center, delta)
+
+        cv2.imshow("PCA", frame)
+        if cv2.waitKey(100) == 27:
+            break
+    
+    cv2.destroyAllWindows()
